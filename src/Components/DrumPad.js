b01f9db6ac7031ca8d6b804e92392455a5aace7b
untari <sastraembun0306@gmail.com>
2021-07-21 23:59:37 +0700

    create styles for drumpad

--- src/Components/DrumPad.js
+++ src/Components/DrumPad.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+const activeStyle = {
+    backgroundColor: '#32a852',
+    boxShado: '0 3px #334d3a',
+    height: 77,
+    marginTop: 13
+};
+
+const inactiveStyle = {
+    backgroundColor: 'grey',
+    marginTop: 10,
+    boxShado: '3px 3px 5px black'
+};
+
 class DrumPad extends Component {
     render() {
         return(
-            <div className="container" id="drum-pad">
+            <div className="drum-pad" id="drum-pad">
                 <h2>Drum Pad</h2>
             </div>
         );
