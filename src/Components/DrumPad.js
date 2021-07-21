37df80cc955cfb2d245ecd85c810d88b11cd8c51
untari <sastraembun0306@gmail.com>
2021-07-21 23:46:05 +0700

    create drumpad and pad bank components

---
+++ src/Components/DrumPad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DrumPad extends Component {
+    render() {
+        return(
+            <div className="container" id="drum-pad">
+                <h2>Drum Pad</h2>
+            </div>
+        );
+    }
+};
+
+export default DrumPad;
---
+++ src/Components/PadBank.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PadBank extends Component {
+    render() {
+        return(
+            <div className="container" id="drum-pad">
+                <h2>PadBank</h2>
+            </div>
+        );
+    }
+};
+
+export default PadBank;
