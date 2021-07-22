f19ad5c34a40d3c3be1130fb636a2f062f1826e6
untari <sastraembun0306@gmail.com>
2021-07-22 13:26:37 +0700

    adding audio

--- src/Components/DrumPad.js
+++ src/Components/DrumPad.js
@@ -22,6 +22,12 @@ class DrumPad extends Component {
             this.handleKeyPress = this.handleKeyPress.bind(this);
             this.activePad = this.activePad.bind(this);
     }
+    componentDidMount(){
+        document.addEventLIstener('keydown', this.handleKeyPress);
+    }
+    componentWillUnmount(){
+        document.removeEventLIstener('keydown', this.handleKeyPress);
+    }
     
     handleKeyPress(e){
         if (e.keyCode === this.props.keyCode){
@@ -29,38 +35,54 @@ class DrumPad extends Component {
         }
     }
     
-    activePad() {
-        if (this.props.power){
-            if(this.state.padStyle.backgroundColor === 'orange')
-                this.seState({
-                    padStyle: inactiveStyle
-                });
+    activatePad() {
+        if (this.props.power) {
+        if (this.state.padStyle.backgroundColor === 'orange') {
+            this.setState({
+            padStyle: inactiveStyle
+            });
         } else {
             this.setState({
-                padStyle: activeStyle
+            padStyle: activeStyle
             });
         }
-    } else if (this.state.padStyle.marginTop === 13){
+        } else if (this.state.padStyle.marginTop === 13) {
         this.setState({
             padStyle: inactiveStyle
         });
-    } else {
+        } else {
         this.setState({
             padStyle: {
-                height: 77,
-                marginTop: 13,
-                backgroundColor: 'grey',
-                boxShadow: '0 3px grey'
+            height: 77,
+            marginTop: 13,
+            backgroundColor: 'grey',
+            boxShadow: '0 3px grey'
             }
         });
+        }
     }
-
+    
+    playSound(){
+        const sound = document.getElementById(this.props.keyTrigger);
+        sound.currentTime = 0;
+        sound.play();
+        this.activetePad();
+        setTimeout(() => this.activetePad(), 100);
+        this.props.updateDisplay(this.props.clipId(/-/g, ' '));
+    }
     render() {
         return(
             <div className="drum-pad" 
-                id="drum-pad"
+                id={this.props.clipId}
+                onClick={this.playSound}
                 style={this.state.padStyle}>
                 
+                <audio 
+                    className='clip'
+                    id={this.props.keyTrigger}
+                    src={this.props.clip}
+                />
+                {this.props.keyTrigger}
             </div>
         );
     }
